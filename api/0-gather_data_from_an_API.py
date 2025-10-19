e1e4a1841e5e1ccf416e9ad57b4f8f9229dabb95
divine150-coder <d.mutesi1@alustudent.com>
2025-10-19 22:08:42 +0000

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,53 +1,36 @@
 #!/usr/bin/python3
-"""
-This script retrieves TODO list progress for a given employee ID
-from the JSONPlaceholder REST API and displays it in a formatted way.
-"""
+"""Script to get todos for a user from API"""
 
 import requests
 import sys
 
 
-def get_employee_todos(employee_id):
-    """Fetch employee info and TODO list from JSONPlaceholder API."""
-    base_url = "https://jsonplaceholder.typicode.com"
-    
-    # Get employee info
-    user_response = requests.get("{}/users/{}".format(base_url, employee_id))
-    if user_response.status_code != 200:
-        print("Error: Employee with ID {} not found.".format(employee_id))
-        return
+def main():
+    """main function"""
+    user_id = int(sys.argv[1])
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
 
-    user = user_response.json()
-    employee_name = user.get("name")
+    response = requests.get(todo_url)
 
-    # Get TODO list
-    todos_response = requests.get("{}/todos?userId={}".format(base_url, employee_id))
-    if todos_response.status_code != 200:
-        print("Error: Could not fetch TODO list for {}.".format(employee_name))
-        return
+    total_questions = 0
+    completed = []
+    for todo in response.json():
 
-    todos = todos_response.json()
-    total_tasks = len(todos)
-    done_tasks = [todo for todo in todos if todo.get("completed") is True]
+        if todo['userId'] == user_id:
+            total_questions += 1
 
-    # Print progress
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, len(done_tasks), total_tasks))
-    for task in done_tasks:
-        print("\t {}".format(task.get("title")))
+            if todo['completed']:
+                completed.append(todo['title'])
+
+    user_name = requests.get(user_url).json()['name']
+
+    printer = ("Employee {} is done with tasks({}/{}):".format(user_name,
+               len(completed), total_questions))
+    print(printer)
+    for q in completed:
+        print("\t {}".format(q))
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-
-    try:
-        emp_id = int(sys.argv[1])
-    except ValueError:
-        print("Error: employee_id must be an integer.")
-        sys.exit(1)
-
-    get_employee_todos(emp_id)
-
+if __name__ == '__main__':
+    main()
